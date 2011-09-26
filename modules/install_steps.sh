25cbe02ab9aecabdca6d324ffc7d8408f3195204
r1k0 <erick@openchill.org>
2011-09-26 11:35:36 +0200

    /mnt/gentoo/boot tends to show twice in /proc/mounts uniq it and get rid of second umount /mnt/gentoo/boot error message

--- modules/install_steps.sh
+++ modules/install_steps.sh
@@ -499,7 +499,7 @@ finishing_cleanup() {
 
 failure_cleanup() {
     spawn "mv ${logfile} ${logfile}.failed" || warn "could not move ${logfile} to ${logfile}.failed"
-    for mnt in $(awk '{ print $2; }' /proc/mounts | grep ^${chroot_dir} | sort -r); do
+    for mnt in $(awk '{ print $2; }' /proc/mounts | grep ^${chroot_dir} | sort -r | uniq); do
         spawn "umount ${mnt}" || warn "  could not unmount ${mnt}.. "
     done
     for swap in $(awk '/^\// { print $1; }' /proc/swaps); do
